9f7ac6d25c4d641eb2d571c91725c761233c9fa4
Tung Hoang <tungxm113@gmail.com>
2020-09-25 16:04:21 +0700

    Updates

---
+++ precache-manifest.ec84d2a52083ccbec413ceafaa17737b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9332f75cdec97714873789ca9e6bb363",
+    "url": "/Royal-Shop/index.html"
+  },
+  {
+    "revision": "25b78796c0d6b545b51b",
+    "url": "/Royal-Shop/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "42013a862c3f0aad792f",
+    "url": "/Royal-Shop/static/css/main.2468bab0.chunk.css"
+  },
+  {
+    "revision": "25b78796c0d6b545b51b",
+    "url": "/Royal-Shop/static/js/2.9d56d4f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Royal-Shop/static/js/2.9d56d4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42013a862c3f0aad792f",
+    "url": "/Royal-Shop/static/js/main.f3f51695.chunk.js"
+  },
+  {
+    "revision": "3a06d64b6b2cd1dfee6e",
+    "url": "/Royal-Shop/static/js/runtime-main.3f329d91.js"
+  }
+]);
